ebc2e0c17c93055d20a37c67ebfa236b4f5dc5d0
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2026-01-14 20:16:17 +0300

    Initialize React blog site project structure
    Add Header component structure and styles

---
+++ react-blogsite/src/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./css/Header.css";
+function Header() {
+  return (
+    <div className="header">
+      <div className="logo">REACT BLOGSİTE</div>
+    </div>
+  );
+}
+
+export default Header;
