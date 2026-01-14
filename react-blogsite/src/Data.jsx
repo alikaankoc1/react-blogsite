0b8592cde2aad157bd9b981a3acb0ffd665287e3
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2026-01-14 20:40:28 +0300

    Create Data.jsx

---
+++ react-blogsite/src/Data.jsx
@@ -0,0 +1,38 @@
+import dpu from "./images/dpu.jpeg";
+import ktu from "./images/firat.jpeg";
+import firat from "./images/ktu.webp";
+import sitki from "./images/sitkikocman.jepg.jpeg";
+const city = [
+  {
+    id: 1,
+    title: "Dumlupınar Üniversitesi",
+    decription:
+      "Kütahya’da bulunan, çeşitli fakülteleriyle eğitim veren bir devlet üniversitesidir.",
+    count: "1992",
+    image: dpu,
+  },
+  {
+    id: 2,
+    title: "Fırat Üniversitesi",
+    decription:
+      "azığ’da yer alan, mühendislik ve sağlık alanlarında öne çıkan köklü bir üniversitedir.",
+    count: "1975",
+    image: firat,
+  },
+  {
+    id: "3",
+    title: "Karadeniz Teknik Üniversitesi",
+    decription:
+      "Trabzon’da bulunan, Türkiye’nin en eski teknik üniversitelerinden biridir.",
+    count: "1955",
+    image: ktu,
+  },
+  {
+    id: "4",
+    title: "Sıtkı Koçman Üniversitesi",
+    decription:
+      "Muğla’da yer alan, geniş kampüsü ve çeşitli bölümleriyle bilinen bir devlet üniversitesidir.f",
+    count: "1992",
+    image: sitki,
+  },
+];
